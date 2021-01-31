e9a3abfc7cdad4add5385450f2891f54db7c76a0
FeniDepani <67674609+FeniDepani@users.noreply.github.com>
2021-01-31 15:16:06 -0800

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var element = /*#__PURE__*/React.createElement("div", {
+  className: "container"
+}, /*#__PURE__*/React.createElement("div", {
+  className: "container-inner-wrapper"
+}, /*#__PURE__*/React.createElement("h1", null, "Fenikumari Kiritbhai Depani"), /*#__PURE__*/React.createElement("div", {
+  className: "pic"
+}), /*#__PURE__*/React.createElement("p", null, "My name is Fenikumari Kiritbhai Depani, I am doing my Masters of Science degree in Computer Science department. I am interested in web development and leraning new technologies to enhance my knowledge. I have worked in TCS for 2.5 years as a web developer. I love to cook new vegetarian cuisines."), /*#__PURE__*/React.createElement("a", {
+  className: "button",
+  href: "https://github.com/FeniDepani"
+}, "VIEW MY GITHUB REPO")));
+ReactDOM.render(element, document.getElementById('content'));
