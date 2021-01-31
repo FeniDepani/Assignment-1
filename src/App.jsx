109b43b3147e81dcff9701f8852c5b0259389f51
FeniDepani <67674609+FeniDepani@users.noreply.github.com>
2021-01-31 15:17:00 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const element = (
+    <div className="container">
+        <div className="container-inner-wrapper">
+            <h1>Fenikumari Kiritbhai Depani</h1>
+            <div  className="pic"></div>
+            <p>My name is Fenikumari Kiritbhai Depani, I am doing my Masters of Science degree in Computer Science department. I am interested in web development and leraning new technologies to enhance my knowledge. I have worked in TCS for 2.5 years as a web developer. I love to cook new vegetarian cuisines.</p>
+            <a className="button" href="https://github.com/FeniDepani">VIEW MY GITHUB REPO</a>
+        </div>
+    </div>
+);
+
+ReactDOM.render(element,document.getElementById('content'));
